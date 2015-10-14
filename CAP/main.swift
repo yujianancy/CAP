7463c2e90169d55796030ed2aefe0934ba83e14b
yujianancy <yujia151992@gmail.com>
2015-10-13 19:44:50 -0700

    Added extra

--- CAP/main.swift
+++ CAP/main.swift
@@ -1,102 +1,126 @@
-//
-//  main.swift
-//  CAP
-//
-//  Created by iGuest on 10/12/15.
-//  Copyright (c) 2015 Jia Yu. All rights reserved.
-//
-
 import Foundation
 
-println("Enter an expression separated by returns:")
-
-func input() -> String {
+func add(number1:Int?, number2:Int?) ->Int?{
     
-    let standardInput = NSFileHandle.fileHandleWithStandardInput()
-    
-    let inputData = standardInput.availableData
-    
-    let strData = NSString(data: inputData, encoding: NSUTF8StringEncoding) as! String
-    
-    return strData.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
-}
-
-func convertInt(incoming:String) -> Int {
-    
-    return NSNumberFormatter().numberFromString(incoming)!.integerValue
-    
-}
-
-func add(number1:Int, number2:Int) ->Int{
-    
-    var result:Int = number1 + number2
-    
-    return result
-    
-}
-
-func sub(number1:Int, number2:Int) ->Int{
-    
-    var result:Int = number1 - number2
-    
-    return result
-    
-}
-
-func mul(number1:Int, number2:Int) ->Int{
-    
-    var result:Int = number1 * number2
-    
-    return result
-    
-}
-
-func div(number1:Int, number2:Int) ->Double{
-    
-    var result:Double = Double(number1 / number2)
-    
-    return result
-    
-}
-
-func calc2ints(number1:Int, number2:Int, op:String) -> Double {
-    
-    var result:Double = 0.0
-    
-    switch op {
+    if number1 != nil && number2 != nil {
         
-    case "add", "+":
-        
-        result = Double(number1 + number2)
-        
-    case "sub", "-":
-        
-        result = Double(number1 - number2)
-        
-    case "mul", "*":
-        
-        result = Double(number1 * number2)
-        
-    case "div", "/":
-        
-        result = Double(number1 / number2)
-    
-    default:
-    
-        println("I don't understand.")
+        return number1! + number2!
         
     }
     
-    return result
+    return nil
+    
 }
 
-func calcArrAdd(numbers:[Int]) ->Int {
+var test = add(3,5)
+
+println(test)
+
+func sub(number1:Int?, number2:Int?) ->Int?{
+    
+    if number1 != nil && number2 != nil {
+        
+        return number1! - number2!
+        
+    }
+    
+    return nil
+    
+}
+
+test = sub(3,4)
+
+println(test)
+
+func mul(number1:Int?, number2:Int?) ->Int?{
+    
+    if number1 != nil && number2 != nil {
+        
+        return number1! * number2!
+        
+    }
+    
+    return nil
+    
+}
+
+test = mul(3,4)
+
+println(test)
+
+func divi(number1:Int?, number2:Int?) ->Double?{
+    
+    if number1 != nil && number2 != nil {
+        
+        return Double(number1!) / Double(number2!)
+        
+    }
+    
+    return nil
+    
+}
+
+var test1 = divi(4,3)
+
+println(test1)
+
+func calc2ints(number1:Int?, number2:Int?, op:String?) -> Double?{
+    
+    var result:Double = 0.0
+    
+    if number1 != nil && number2 != nil && op != nil {
+        
+        switch op! {
+            
+        case "add", "+":
+            
+            result = Double(number1! + number2!)
+            
+        case "sub", "-":
+            
+            result = Double(number1! - number2!)
+            
+        case "mul", "*":
+            
+            result = Double(number1! * number2!)
+            
+        case "div", "/":
+            
+            result = Double(number1!)/Double(number2!)
+            
+        default:
+            
+            print("I don't understand.")
+            
+        }
+        
+        return result
+        
+    }
+    
+    return nil
+    
+}
+
+test1 = calc2ints(3, 4, "sub")
+
+println(test1)
+
+func calcArrAdd(numbers:[Int?]) ->Int?{
     
     var result = 0
     
     for index in 0...numbers.count-1{
         
-        result = result + numbers[index]
+        if numbers[index] == nil {
+            
+            return nil
+            
+        } else {
+            
+            result = result + numbers[index]!
+            
+        }
         
     }
     
@@ -104,13 +128,25 @@ func calcArrAdd(numbers:[Int]) ->Int {
     
 }
 
-func calcArrMul(numbers:[Int]) ->Int {
+test = calcArrAdd([1,2,3,4,5])
+
+println(test)
+
+func calcArrMul(numbers:[Int?]) ->Int? {
     
     var result = 1
     
     for index in 0...numbers.count-1{
         
-        result = result * numbers[index]
+        if numbers[index] == nil {
+            
+            return nil
+            
+        } else {
+            
+            result = result * numbers[index]!
+            
+        }
         
     }
     
@@ -118,32 +154,138 @@ func calcArrMul(numbers:[Int]) ->Int {
     
 }
 
-func calcArrCount(numbers:[Int]) ->Int {
+test = calcArrMul([1,2,3,4,5])
+
+println(test)
+
+func calcArrCount(numbers:[Int?]) ->Int? {
     
-    var result = numbers.count - 1
+    for index in 0...numbers.count-1{
+        
+        if numbers[index] == nil {
+            
+            return nil
+        }
+    }
     
-    return result
+    return numbers.count
     
 }
 
-func calcArrAvg(numbers:[Int]) ->Double {
+test = calcArrCount([1,2,3,4,5])
+
+println(test)
+
+func calcArrAvg(numbers:[Int?]) ->Double? {
     
     var result = 0.0
     
     for index in 0...numbers.count-1{
         
-        result = result + Double(numbers[index])
+        if numbers[index] == nil {
+            
+            return Double(nilHandleErr)
+            
+        } else {
+            
+            result = result + Double(numbers[index]!)
+            
+        }
         
     }
     
-    return result/Double(numbers.count-1)
+    return result/Double(numbers.count)
     
 }
 
-func calcArrAll
-var inputStr = input()
+test1 = calcArrAvg([1,2,3,4,5])
 
+println(test1)
 
+func calcArrAll(numbers:[Int?], op:String!) ->Double? {
+    
+    var result:Double = 0.0
+    
+    for index in 0...numbers.count-1{
+        
+        if numbers[index] == nil {
+            
+            return Double(nilHandleErr)
+            
+        } else {
+            
+            switch op {
+                
+            case "add", "+":
+                
+                for index in 0...numbers.count-1{
+                    
+                    result = result + Double(numbers[index]!)
+                    
+                }
+                
+            case "mul", "*":
+                
+                result = 1.0
+                
+                for index in 0...numbers.count-1{
+                    
+                    result = result * Double(numbers[index]!)
+                    
+                }
+                
+            case "count":
+                
+                result = Double(numbers.count)
+                
+            case "avg":
+                
+                for index in 0...numbers.count-1{
+                    
+                    result = result + Double(numbers[index]!)
+                    
+                }
+                
+                return result/Double(numbers.count)
+                
+            default:
+                
+                print("I don't understand.")
+                
+            }
+            
+        }
+        
+    }
+    
+    return result
+    
+}
 
+test1 = calcArrAll([1,2,3,4,2],"count")
 
+println(test1)
 
+func addPoint(point1:(Double,Double), point2:(Double,Double)) -> (Double, Double){
+    
+    let result = ((point1.0 + point2.0), (point1.1 + point2.1))
+    
+    return result
+    
+}
+
+var test2 = addPoint((1,2.2),(2.5,3))
+
+println(test2)
+
+func subPoint(point1:(Double,Double), point2:(Double,Double)) -> (Double, Double){
+    
+    let result = ((point1.0 - point2.0), (point1.1 - point2.1))
+    
+    return result
+    
+}
+
+test2 = subPoint((1,2.2),(2.5,3))
+
+println(test2)
